89fa8ef5baa311869fdf7da2ab3de67f1681c82e
sharif-08 <otwist731@gmail.com>
2025-03-09 13:09:37 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,8 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+print("original list: ",list)
+
+list1=list[:5]
+print("extracted first five element:",list1)
+
+reverse=list1[::-1]
+print("reverser first five list: ",reverse)
